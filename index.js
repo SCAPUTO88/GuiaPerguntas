540f1357f77d339a7e0b0005c899428ec0b20670
SCAPUTO88 <sandro.caputo@gmail.com>
2022-11-03 01:13:34 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+
+//estou dizendo para o express usar o EJS como view engine
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/perguntar", (req, res) => {
+  res.render("perguntar");
+});
+
+app.listen(8080, () => {
+  console.log("app rodando");
+});
